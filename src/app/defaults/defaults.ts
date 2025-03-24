e18f8b06a3f7203272e993b839089510410b6be8
Artem Rasskazimov <sozhran@gmail.com>
2025-03-24 18:43:41 +0100

    saving progress

--- src/app/defaults/defaults.ts
+++ src/app/defaults/defaults.ts
@@ -35,19 +35,39 @@ export const defaultItemStyle = { width: "auto", height: "auto" };
 
 export const defaultNumberOfItems = 5;
 
-const containerOptions = {
+export type containerOptionsType = {
+	flex: string[];
+	flexDirection: string[],
+	flexWrap: string[],
+	justifyContent: string[],
+	alignItems: string[],
+	alignContent: string[],
+	width: string[],
+	height: string[],
+	gap: string[]
+};
+
+export type itemOptionsType = {
+	width: string[],
+	height: string[],
+	flexGrow: string[],
+	flexShrink: string[],
+	flexBasis: string[]
+};
+
+export const containerOptions: any = {
 	flex: ["flex", "inline-flex", "block"],
 	flexDirection: ["row", "row-reverse", "column", "column-reverse"],
 	flexWrap: ["nowrap", "wrap", "wrap-reverse"],
 	justifyContent: ["start", "center", "end", "space-between", "space-around", "space-evenly", "stretch"],
 	alignItems: ["start", "center", "end", "stretch"],
-	alignContent: ["", "", "", "", "", ""],
+	alignContent: ["start", "center", "end", "space-between", "space-around", "space-evenly", "stretch"],
 	width: ["auto", "max-content", "min-content", "fit-content", "0", "100%"],
 	height: ["auto", "max-content", "min-content", "fit-content", "0", "100%"],
 	gap: ["0"],
 };
 
-const itemOptions = {
+export const itemOptions: any = {
 	width: ["auto", "max-content", "min-content", "fit-content", "0", "100%"],
 	height: ["auto", "max-content", "min-content", "fit-content", "0", "100%"],
 	flexGrow: ["0", "1", "2"],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,14 @@
 //import ItemControls from "./components/ItemControls";
 import { useState, createContext, useContext } from "react";
 import ReactDOM from "react-dom/client";
-import { defaultContainerStyle, defaultItemStyle, defaultNumberOfItems } from "./defaults/defaults";
+import {
+	containerOptions,
+	itemOptions,
+	defaultContainerStyle,
+	defaultItemStyle,
+	defaultNumberOfItems,
+	containerOptionsType,
+} from "./defaults/defaults";
 
 //const [containerStyle, setContainerStyle] = useState<Object>(defaultContainerStyle);
 //const [itemStyle, setItemStyle] = useState<Object>(defaultItemStyle);
@@ -30,7 +37,8 @@ const defaultTestStyle: defaultStyle = {
 
 export default function Home() {
 	const [numberOfItems, setNumberOfItems] = useState<number>(5);
-	const [testStyle, setTestStyle] = useState<any>(JSON.parse(JSON.stringify(defaultTestStyle)));
+	const [containerStyle, setContainerStyle] = useState<any>(JSON.parse(JSON.stringify(defaultTestStyle)));
+	const [itemStyle, setItemStyle] = useState<any>(JSON.parse(JSON.stringify(defaultTestStyle)));
 
 	const changeStyle = (parameter: string, value: string) => {
 		const xxx = { ...testStyle };
@@ -48,59 +56,50 @@ export default function Home() {
 	return (
 		<>
 			{/*<StyleContext.Provider value={containerStyle}>*/}
-			<main className="main-flex-window">
-				<div className="flexie" style={testStyle}>
-					<div className="test-square">1</div>
-					<div className="test-square">2</div>
-					<div className="test-square">3</div>
-					<div className="test-square">4</div>
-					{/*<ContainerControls />*/}
-					{/*<Playground />*/}
-					{/*<ItemControls />*/}
+			<main>
+				<div className="main-controls">
+					<p>CONTAINER SETTINGS</p>
+					{Object.keys(containerOptions).map((key) => {
+						return (
+							<p>
+								<span>{key + ": "}</span>
+								{containerOptions[key].map((elm: string) => {
+									return <button onClick={() => changeStyle(key, elm)}>{elm}</button>;
+								})}
+							</p>
+						);
+					})}
+				</div>
+				<div className="main-flex-window">
+					<div className="flexie" style={testStyle}>
+						<div className="test-square">1</div>
+						<div className="test-square">2</div>
+						<div className="test-square">3</div>
+						<div className="test-square">4</div>
+					</div>
+				</div>
+				<div className="main-controls">
+					<p>ITEM SETTINGS</p>
+					{Object.keys(itemOptions).map((key) => {
+						return (
+							<p>
+								<span>{key + ": "}</span>
+								{itemOptions[key].map((elm: string) => {
+									return <button onClick={() => changeStyle(key, elm)}>{elm}</button>;
+								})}
+							</p>
+						);
+					})}
+
+					<p>
+						<button onClick={() => setTestStyle(defaultTestStyle)}>DEFAULT</button>
+
+						<button onClick={() => changeStyle("alignItems", "center")}>Align Items Center</button>
+						<button onClick={() => console.log(testStyle)}>CONSOLE LOG</button>
+					</p>
 				</div>
 			</main>
-			<p>
-				JUSTIFY-CONTENT: <button onClick={() => changeStyle("justifyContent", "start")}>start</button>
-				<button onClick={() => changeStyle("justifyContent", "center")}>center</button>
-				<button onClick={() => changeStyle("justifyContent", "end")}>end</button>
-				<button onClick={() => changeStyle("justifyContent", "space-between")}>space-between</button>
-				<button onClick={() => changeStyle("justifyContent", "space-around")}>space-around</button>
-				<button onClick={() => changeStyle("justifyContent", "space-evenly")}>space-evenly</button>
-				<button onClick={() => changeStyle("justifyContent", "stretch")}>stretch</button>
-			</p>
-			<p>
-				ALIGN-ITEMS: <button onClick={() => changeStyle("alignItems", "start")}>start</button>
-				<button onClick={() => changeStyle("alignItems", "center")}>center</button>
-				<button onClick={() => changeStyle("alignItems", "end")}>end</button>
-				<button onClick={() => changeStyle("alignItems", "space-between")}>space-between</button>
-				<button onClick={() => changeStyle("alignItems", "space-around")}>space-around</button>
-				<button onClick={() => changeStyle("alignItems", "space-evenly")}>space-evenly</button>
-				<button onClick={() => changeStyle("alignItems", "stretch")}>stretch</button>
-			</p>
-			<p>
-				WIDTH: <button onClick={() => changeStyle("alignItems", "start")}>start</button>
-				<button onClick={() => changeStyle("alignItems", "center")}>center</button>
-				<button onClick={() => changeStyle("alignItems", "end")}>end</button>
-				<button onClick={() => changeStyle("alignItems", "space-between")}>space-between</button>
-				<button onClick={() => changeStyle("alignItems", "space-around")}>space-around</button>
-				<button onClick={() => changeStyle("alignItems", "space-evenly")}>space-evenly</button>
-				<button onClick={() => changeStyle("alignItems", "stretch")}>stretch</button>
-			</p>
-			<p>
-				HEIGHT: <button onClick={() => changeStyle("alignItems", "start")}>start</button>
-				<button onClick={() => changeStyle("alignItems", "center")}>center</button>
-				<button onClick={() => changeStyle("alignItems", "end")}>end</button>
-				<button onClick={() => changeStyle("alignItems", "space-between")}>space-between</button>
-				<button onClick={() => changeStyle("alignItems", "space-around")}>space-around</button>
-				<button onClick={() => changeStyle("alignItems", "space-evenly")}>space-evenly</button>
-				<button onClick={() => changeStyle("alignItems", "stretch")}>stretch</button>
-			</p>
-			<p>
-				<button onClick={() => setTestStyle(defaultTestStyle)}>DEFAULT</button>
 
-				<button onClick={() => changeStyle("alignItems", "center")}>Align Items Center</button>
-				<button onClick={() => console.log(testStyle)}>CONSOLE LOG</button>
-			</p>
 			{/*</StyleContext.Provider>*/}
 		</>
 	);
